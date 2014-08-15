b98f8f828a2c462f032a93534456aafe1c68dfd6
Ele Palmer <ecpalmer21@gmail.com>
2014-08-15 15:24:11 -0400

    assignment 7 complete

--- js/app.js
+++ js/app.js
@@ -8,11 +8,18 @@ $(document).ready(function() {
         $(".ryu-still").show();
     })
     .mousedown(function() {
-        // play hadouken sound
+        playHadouken();
         $(".ryu-ready").hide();
         $(".ryu-throwing").show();
-        $(".hadouken").show();
-        // animate houdoken to right of screen
+        $(".hadouken").finish().show()
+        .animate(
+            {"left": "300px"},
+            500,
+            function() {
+                $(this).hide();
+                $(this).css("left", "-212px");
+            }
+        );
     })
     .mouseup(function() {
         $(".ryu-throwing").hide();
@@ -20,3 +27,8 @@ $(document).ready(function() {
     });
 })
     
+function playHadouken() {
+    $("#hadouken-sound")[0].volume = 0.5;
+    $("#hadouken-sound")[0].load();
+    $("#hadouken-sound")[0].play();
+}
